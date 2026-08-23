9b0a42fd0316357b43672ce101ee7a75e59cacf0
Praj241998 <prajwalbgowda1@gmail.com>
2026-08-23 22:39:51 +0530

    First commit

---
+++ FirstDemo/src/com/training/ArrayExample2.java
@@ -0,0 +1,18 @@
+package com.training;
+
+public class ArrayExample2 {
+
+	public static void main(String[] args) {
+	int [] arr= {1,2,3,4,5,6,7};
+	int sum=0;
+	
+	for(int val:arr)
+	{
+		sum=sum+val;
+	
+	}
+	System.out.println(sum);
+
+	}
+
+}
---
+++ FirstDemo/src/com/training/Demo.java
@@ -0,0 +1,12 @@
+package com.training;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.print("Hii bro\n");
+		System.out.println("Hii Java");
+		System.out.println("\"Hiii Java\"");
+
+	}
+
+}
